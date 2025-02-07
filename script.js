33fbeffdeba1c9bdf122998cc95247960e04a5d0
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-07 19:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	
+		let name = document.getElementById("first").value.trim();
+		let last = document.getElementById("last").value.trim();
+		alert(`${name} ${last}`);
+	
+	
+	
 
 }
